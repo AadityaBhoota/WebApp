858e5bb66e99e3a4301f8b40b3bca77bbccf98a0
AadityaBhoota <aadityabhoota@gmail.com>
2023-07-25 00:24:30 -0400

    initial commit timestamp 13:49

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import bodyParser from "body-parser"
+import mongoose from "mongoose";
+import cors from "cors";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+import exp from "constants";
+
+/* CONFIGURATIONS */
+const __filename = fileURLtoPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+doteenv.config();
+const app = express();
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }))
+
+
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+// app.use(bodyParser.json({ limit: "30mb", extended: true}));
+// app.use(bodyParser)
+app.use(cors());
+app.use("/assests", express.static(path.join(__dirname, 'public/assets')));
